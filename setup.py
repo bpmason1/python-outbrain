9f55aed09e39be30da766a486db40951e6eea38d
Brian Mason <bpmason1@users.noreply.github.com>
2015-04-13 15:06:16 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -4,34 +4,21 @@ except ImportError:
     from distutils.core import setup
 import os
 
-this_dir = os.path.dirname(__file__)
-readme_filename = os.path.join(this_dir, 'README.md')
-requirements_filename = os.path.join(this_dir, 'requirements.txt')
-
-PACKAGE_NAME = 'python-outbrain'
-PACKAGE_VERSION = '0.0.1'
-PACKAGE_AUTHOR = 'Alan Anders'
-PACKAGE_AUTHOR_EMAIL = ''
-PACKAGE_URL = 'https://github.com/andessen/python-outbrain'
-PACKAGES = ['outbrain']
-PACKAGE_LICENSE = 'LGPL 2.1 or later'
-PACKAGE_DESCRIPTION = 'Wrapper for the Outbrain Amplify API'
-
-with open(readme_filename) as f:
-    PACKAGE_LONG_DESCRIPTION = f.read()
-
-with open(requirements_filename) as f:
+with open('requirements.txt') as f:
     PACKAGE_INSTALL_REQUIRES = [line[:-1] for line in f]
 
+with open('README.md') as file:
+    PACKAGE_LONG_DESCRIPTION = file.read()
+
 setup(
-    name=PACKAGE_NAME,
-    version=PACKAGE_VERSION,
-    author=PACKAGE_AUTHOR,
-    author_email=PACKAGE_AUTHOR_EMAIL,
-    url=PACKAGE_URL,
-    packages=PACKAGES,
-    license=PACKAGE_LICENSE,
-    description=PACKAGE_DESCRIPTION,
+    name='outbrain',
+    version='0.0.2',
+    author='Alan Anders',
+    author_email='aanders@simplereach.com',
+    url='https://github.com/andessen/python-outbrain',
+    packages=['outbrain'],
+    license='LGPL 2.1 or later',
+    description='Wrapper for the Outbrain Amplify API',
     long_description=PACKAGE_LONG_DESCRIPTION,
     install_requires=PACKAGE_INSTALL_REQUIRES,
 )

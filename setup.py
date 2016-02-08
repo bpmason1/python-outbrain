01799965f283fbcbd5b8aab8196c5af30af53559
Brian Mason <bmason@simplereach.com>
2016-02-08 10:16:27 -0500

    remove requiremets files (requirements are in setup.py)

--- setup.py
+++ setup.py
@@ -3,6 +3,19 @@ try:
 except ImportError:
     from distutils.core import setup
 
+INSTALL_REQUIRE = [
+    'pytz >= 2013.6',
+    'pyyaml >= 3.10',
+    'requests >= 2.4.3',
+    'simplejson >= 3.1.0',
+]
+
+TEST_REQUIRE = [
+    'mock >= 1.0.1',
+    'nose >= 1.3.6',
+    'pep8 >= 1.6.2',
+]
+
 setup(
     name='outbrain',
     version='0.0.10',
@@ -12,12 +25,8 @@ setup(
     packages=['outbrain'],
     license='LGPL 2.1 or later',
     description='Wrapper for the Outbrain Amplify API',
-    install_requires=[
-        'pytz >= 2013.6',
-        'pyyaml >= 3.10',
-        'requests >= 2.4.3',
-        'simplejson >= 3.1.0',
-    ],
+    install_requires=INSTALL_REQUIRE,
+    tests_require=TEST_REQUIRE,
     keywords=['outbrain','api', 'amplify'],
     classifiers=['Intended Audience :: Developers']
 )
